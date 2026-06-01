c10b35e64a00acb64459802b93242c48548b4e3d
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2026-06-01 15:16:43 +0200

    Add animated quadrant character sequence example with UTF-8 encoding

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,7 +1,46 @@
 ﻿
-using LasseVK.Console;
+using System.Text;
 
-for (int i = 0; i <= 100; i++)
+using LasseVK.Console;
+using LasseVK.Console.Ansi;
+
+System.Console.OutputEncoding = Encoding.UTF8;
+
+var line = new ConsoleLine();
+
+const char upperLeftQuadrant = '\u259b';
+const char upperRightQuadrant = '\u259c';
+const char lowerLeftQuadrant = '\u2599';
+const char lowerRightQuadrant = '\u259f';
+const char upperLeft = '\u2598';
+const char upperRight = '\u259d';
+const char lowerLeft = '\u2596';
+const char lowerRight = '\u2597';
+const char lowerHalf = '\u2584';
+const char upperHalf = '\u2580';
+const char leftHalf = '\u258c';
+const char rightHalf = '\u2590';
+
+string[] frames = [
+    "" + upperLeftQuadrant + " ",
+    "" + leftHalf + " ",
+    "" + lowerLeftQuadrant + " ",
+    "" + lowerHalf + " ",
+    "" + lowerRight + lowerLeft,
+    " " + lowerHalf,
+    " " + lowerRightQuadrant,
+    " " + rightHalf,
+    " " + upperRightQuadrant,
+    " " + upperHalf,
+    "" + upperRight + upperLeft,
+    "" + upperHalf + " ",
+];
+int index = 0;
+
+while (true)
 {
-    Console.WriteLine($"{i:000}: {ProgressBar.Format(i, 100)}");
+    line.Text = frames[index] + " " + index;
+    index = (index + 1) % frames.Length;
+
+    await Task.Delay(50);
 }
